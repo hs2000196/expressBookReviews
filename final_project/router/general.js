684023b417f36f7846c83d8ba87328c7213f2931
hs2000196 <154812314+hs2000196@users.noreply.github.com>
2024-02-13 21:24:29 +0000

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,93 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(username);
+    console.log(password);
+    console.log(req.body);
+    return res.status(400).json({ error: "sd" });
+  if (!username && username.length > 1)
+    return res.status(400).json({ error: "name is required" });
+  if (!password &&  password.length > 1)
+    return res.status(400).json({error: "password is required"});
+
+  // check if username is already exist or not
+  //if (!isValid(username))
+  //  return res.status(400).json({ error: "user is already exist" });
+//task 6
+  users.push({username: username,password: password,});
+  res.status(200).json({ message: "user registered successfully.", users });
+});
+// Task 2
+const isbn = req.params.isbn
+	let foundBook = findBookBy(constants.ISBN, isbn)
+	if (foundBook)
+    	res
+            .status(200)
+            .json({ message: "Book found", data: foundBook})
+	else
+		res
+			.status(400)
+			.json({ message: "Bad request params", data: null });
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const book_need = req.params.isbn;
+    res.send(books[book_need]);
+    
  });
-  
+  //task 3
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/author/:author", function (req, res) {
+    const author = req.params.author
+	const foundBook = findBookBy(constants.AUTHOR, author)
+	if (foundBook)
+		res
+			.status(200)
+			.json({ message: 'Book found', data: foundBook })
+	else
+		res
+			.status(400)
+			.json({ message: "Bad request params", data: null })
+});
+    
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+// Task 4
+// Get a book based on its title synchronously
+public_users.get("/title/:title", function (req, res) {
+	const title = req.params.title
+	const foundBook = findBookBy(constants.TITLE, title)
+	if (foundBook)
+		res
+			.status(200)
+			.json({ message: 'Book found', data: foundBook })
+	else
+		res
+			.status(400)
+			.json({ message: "Bad request params", data: null })
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn
+	const foundBook = findBookBy(constants.ISBN, isbn)
+	const reviews = foundBook?.reviews
+
+	if (reviews)
+		res
+			.status(200)
+			.json({ message: 'Reviews found', data: reviews })
+	else
+		res
+			.status(400)
+			.json({ message: "Bad request params", data: null })
 });
 
 module.exports.general = public_users;
+
